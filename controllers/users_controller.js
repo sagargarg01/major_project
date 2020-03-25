2c8ecb4277682d8493c7d6031cefcbee53442613
Sagar Garg <sagargupta1417@gmail.com>
2020-03-25 19:51:07 +0530

    added sign-in and sign-up to the page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,31 @@ module.exports.profile = function(req,res){
         title : 'Users'
     });
 }
+
+
+// render the sign up page
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title : "Codeial | Sign Up"
+    });
+}
+
+
+// render the sign in page
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title : "Codeial | Sign In"
+    });
+}
+
+
+// get the signup data
+module.exports.create = function(req,res){
+    // todo later
+}
+
+
+//sign in and create a session for the user
+module.exports.createSession = function(req,res){
+    //todo later
+}
--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,7 @@ const usersControllers = require('../controllers/users_controller');
 
 router.get('/profile',usersControllers.profile);
 
+router.get('/sign-up',usersControllers.signUp);
+router.get('/sign-in',usersControllers.signIn);
 
 module.exports = router;
