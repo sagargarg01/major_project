7f1b5d19159858262f44f67ae3d641988423f3c7
Sagar Garg <sagargupta1417@gmail.com>
2020-03-31 23:07:29 +0530

    created schema for comments and added it to the database

--- models/post.js
+++ models/post.js
@@ -9,7 +9,13 @@ const postSchema = new mongoose.Schema({
         type:  mongoose.Schema.Types.ObjectId,
         ref: 'User'
 
+    },
+    //include the RRy of ids of all comments in this post Schema itself
+    comments:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'comment'
     }
+]
 },{
     timestamps: true
 });
