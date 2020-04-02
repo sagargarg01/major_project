6683b4586cb68437ac80d4bbcd8cf59ea8cb4264
Sagar Garg <sagargupta1417@gmail.com>
2020-04-02 15:19:37 +0530

    converted code to Async Await, and now post user can delete other user comments on its post too

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,48 +1,59 @@
-const Comment = require('../models/comment');
-const Post = require('../models/post');
+    const Comment = require('../models/comment');
+    const Post = require('../models/post');
 
 
-module.exports.create = function(req, res){
-    Post.findById(req.body.post, function(err,post){
+    module.exports.create = async function(req, res){
 
-        if(post){
-            Comment.create({
-                content: req.body.content,
-                post: req.body.post,
-                user: req.user._id
-            }, function(err,comment){
-                //handle error
+        try{
 
+            let post = await Post.findById(req.body.post);
+
+            if(post){
+                Comment.create({
+                    content: req.body.content,
+                    post: req.body.post,
+                    user: req.user._id
+                });
+    
                 post.comments.push(comment);
                 post.save(); 
-
+    
                 res.redirect('/');
-            });
+            }
+        }catch(err){
+            console.log('Error', err);
+            return;
         }
-    });
-}
 
-module.exports.destroy = function(req, res){
-    Comment.findById(req.params.id, function(err,comment){
-        let usernameid;
-        let postId = comment.post;
-        Post.findById(postId,function(err,post){
-            usernameid = post.user;
-            console.log(post.user, req.user.id);
-        });
+    }
 
-        // console.log(usernameid);
-        if(comment.user == req.user.id || req.user.id == usernameid){
+    module.exports.destroy = async function(req, res){
+        
+        try{
 
+        let comment = await Comment.findById(req.params.id);
+       
+        let post = await Post.findById(comment.post);
+
+         console.log(post.user, req.user.id);
+
+        if(comment.user == req.user.id  || req.user.id == post.user){
+                    
+            let postId = comment.post;
             comment.remove();
 
-            Post.findByIdAndUpdate(postId ,{ $pull: {comments: req.params.id} }, function(err,post){
-            return res.redirect('back');
-            });
+            let post = await Post.findByIdAndUpdate(postId ,{ $pull:
+            {comments: req.params.id} });
 
-        }
-        else{
+            return res.redirect('back');
+        }else{
             return res.redirect('back');
         }
-    });
-}
+        
+
+        }catch(err){
+            console.log('Error', err);
+            return;
+        }
+
+    }
--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,33 +1,43 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-     //req.cookies ka condole log
+     //req.cookies ka console log
 
-module.exports.home = function(req,res){
-    // Post.find({}, function(err,posts){
+module.exports.home = async function(req,res){
+
+    try{
+        //populate the user of each point 
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: { 
+                path: 'user',
+            }
+        });
+
+        let users = await User.find({});
+
+        return res.render('home',{
+            title : "Home",
+            posts : posts,
+            all_users: users
+        });
+    }catch(err){
+        console.log('Error',err);
+        return;
+    }
+   
+   
+}
+
+
+
+
+
+ // Post.find({}, function(err,posts){
     //     return res.render('home',{
     //         title : "Home",
     //         posts : posts
     //     });
     // });
-
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: { 
-            path: 'user',
-        }
-    })
-    .exec(function(err,posts){
-
-        User.find({}, function(err,users){
-            return res.render('home',{
-                title : "Home",
-                posts : posts,
-                all_users: users
-            });
-        })
-        
-    })
-}
