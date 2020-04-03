fb1ba119edf413e5f6c2618287e3c0271b7c4139
Sagar Garg <sagargupta1417@gmail.com>
2020-04-03 14:03:25 +0530

    created post and recieving data using AJAX

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -3,45 +3,54 @@ const Comment = require('../models/comment');
 
 module.exports.create = async function(req, res){
 
-    try{
-       await Post.create({
-            content: req.body.content, 
+    try {
+        let post = await Post.create({
+            content: req.body.content,
             user: req.user._id
         });
-    
-        req.flash('success','Post created successfully');
+
+        if(req.xhr){
+            return res.status(200).json({
+                data: {
+                    post: post
+                },
+                message: 'Post Created!'
+            });
+        }
+
+        req.flash('success', 'Post created successfully');
         return res.redirect('back');
 
-    }catch(err){
+    } catch (err) {
         req.flash('error', err);
         return res.redirect('back');
-        }
+    }
 
 }
 
 
 module.exports.destroy = async function(req, res){
 
-    try{
+    try {
         let post = await Post.findById(req.params.id);
 
-        if(post.user == req.user.id){
+        if (post.user == req.user.id) {
             post.remove();
-    
-          await Comment.deleteMany({post: req.params.id});
 
-          req.flash('success','Post deleted successfully');
-          return res.redirect('back');
-    
-        }else{
-            req.flash('error','Not Authorized to delete');
-           return res.redirect('back');
+            await Comment.deleteMany({ post: req.params.id });
+
+            req.flash('success', 'Post deleted successfully');
+            return res.redirect('back');
+
+        } else {
+            req.flash('error', 'Not Authorized to delete');
+            return res.redirect('back');
         }
 
-    }catch(err){
+    } catch (err) {
         req.flash('error', err);
         return res.redirect('back');
-        }
+    }
 
 
 }
