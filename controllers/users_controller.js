d53e5b43c1c2e37d8296a422780710f8049a38ac
Sagar Garg <sagargupta1417@gmail.com>
2020-04-07 14:08:45 +0530

    user can add a profile picture (still in development)

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const fs = require('fs');
+const path = require('path');
 
 // keep it same as before
 module.exports.profile = function(req,res){
@@ -32,6 +34,10 @@ module.exports.update = async function(req, res){
                    user.email = req.body.email;
 
                    if(req.file){
+
+                      if(user.avatar){
+                          fs.unlinkSync(path.join(__dirname , '..', user.avatar));
+                      }
                        // this is saving the path of the uploaded file into the avatar field in the user
                        user.avatar = User.avatarPath + '/' + req.file.filename;
                    }
--- index.js
+++ index.js
@@ -29,6 +29,9 @@ app.use(cookieParser());
 
 app.use(express.static('./assets'));
 
+// make the uploads path avaialable to the browser
+app.use('/uploads', express.static(__dirname + '/uploads'));
+
 app.use(expressLayouts);
 
 //extract style and scripts from sub pages into the layout
