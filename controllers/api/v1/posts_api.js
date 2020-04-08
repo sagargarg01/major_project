b5bdcbd3b514e7987e3af78f85f2e99b18866562
Sagar Garg <sagargupta1417@gmail.com>
2020-04-09 00:02:02 +0530

    shows the data from database via api

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,56 @@
-module.exports.index = function(req, res){
+const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
+
+module.exports.index = async function (req, res) {
+
+    //populate the user of each point 
+    let posts = await Post.find({})
+        .sort('-createdAt')
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user',
+         }
+    });
+
+
+
+
     return res.json(200, {
         message: 'lists of posts v1',
-        posts: []
-    })
+        posts: posts
+    });
+
+
+}
+
+
+module.exports.destroy = async function(req, res){
+
+    try {
+        let post = await Post.findById(req.params.id);
+
+        // if (post.user == req.user.id) {
+            post.remove();
+
+            await Comment.deleteMany({ post: req.params.id });
+
+    
+            return res.json(200, {
+                message : 'post and associated comments deleted successfully'
+            });
+
+        // } else {
+        //     req.flash('error', 'Not Authorized to delete');
+        //     return res.redirect('back');
+        // }
+
+    } catch (err) {
+        return res.json(500, {
+            message: 'internal server error'
+        });
+    }
+
+
 }
